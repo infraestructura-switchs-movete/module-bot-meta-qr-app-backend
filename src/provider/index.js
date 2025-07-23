ce9d723c03446eab0b9ac9708d48fa0138bee504
JuanAlejandro\Juan Alejandro <juanlondono0208@gmail.com>
2025-07-22 22:22:23 -0500

    archivo config,  variables de entorno configuradas

--- src/provider/index.js
+++ src/provider/index.js
@@ -3,9 +3,9 @@ import { MetaProvider as Provider } from "@builderbot/provider-meta";
 import { config } from "../config/index.js";
 
 console.log("ENV loaded:", {
-  jwtToken: process.env.JWT_TOKEN,
-  numberId: process.env.NUMBER_ID,
-  verifyToken: process.env.VERIFY_TOKEN,
+  jwtToken: config.jwtToken,
+  numberId: config.numberId,
+  verifyToken: config.verifyToken,
 });
 
 export const provider = createProvider(Provider, {
